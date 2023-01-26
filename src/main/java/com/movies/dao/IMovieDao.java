887533f2c0f94b2ebac62928d260735b81340e4f
Renzo Jacinto <rjacinto@fi.uba.ar>
2023-01-26 19:54:48 -0300

    added mysql connection with flyway for migrations

--- src/main/java/com/movies/dao/IMovieDao.java
+++ src/main/java/com/movies/dao/IMovieDao.java
@@ -1,5 +1,13 @@
 package com.movies.dao;
 
-public interface IMovieDao {
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.movies.domain.Movie;
+
+public interface IMovieDao extends JpaRepository<Movie, Long> {
+
+    public List<Movie> findByTitleLike(String title);
     
 }
--- src/main/java/com/movies/services/MovieService.java
+++ src/main/java/com/movies/services/MovieService.java
@@ -3,34 +3,41 @@ package com.movies.services;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.movies.dao.IMovieDao;
 import com.movies.domain.Movie;
 
 @Service
 public class MovieService implements IMovieService {
 
+    @Autowired
+    private IMovieDao movieDao;
+
     @Override
     public void save(Movie movie) {
+        movieDao.save(movie);
     }
 
     @Override
     public void delete(Long id) {
+        movieDao.deleteById(id);
     }
 
     @Override
     public List<Movie> findAll() {
-        return new ArrayList<Movie>();
+        return movieDao.findAll();
     }
 
     @Override
     public Movie findById(Long id) {
-        return new Movie();
+        return movieDao.findById(id).orElse(null);
     }
 
     @Override
     public List<Movie> findByTitle(String title) {
-        return new ArrayList<Movie>();
+        return movieDao.findByTitleLike(title);
     }
     
 }
